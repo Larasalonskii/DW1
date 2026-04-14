68caf65fc08a2a0b51d1dcbe340079b8d40ed249
Larasalonskii <laraszanoni14@gmail.com>
2026-04-14 11:09:50 -0300

    Create dogão.js

---
+++ 1 BIMESTRE/dogão/dogão.js
@@ -0,0 +1,11 @@
+function fcalcular(){
+    let dogbasico = (parseInt(document.getElementById("inputcqb").value) || 0) * 22;
+    let dogduplo = (parseInt(document.getElementById("inputcqd").value) || 0) * 26;
+    let xsalada = (parseInt(document.getElementById("inputxs").value) || 0) * 29;
+    let refrip = (parseInt(document.getElementById("inputr").value) || 0) * 5;
+    let refri1 = (parseInt(document.getElementById("inputr1").value) || 0) * 8;
+
+    let resp = dogbasico + dogduplo + xsalada + refrip + refri1;
+
+    document.getElementById("total").innerHTML = "R$ " + resp.toFixed(2);
+}
