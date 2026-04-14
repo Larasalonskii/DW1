61ff1b55ce5abb7835b64ec4c4d5d3b14839bcd1
Larasalonskii <laraszanoni14@gmail.com>
2026-04-14 11:49:53 -0300

    atualizando dogao

--- 1 BIMESTRE/14-04-2026 Dogão/dogão.js
+++ 1 BIMESTRE/14-04-2026 Dogão/dogão.js
@@ -1,14 +1,23 @@
 function fcalcular(){
-    let dogbasico = (parseInt(document.getElementById("inputcqb").value) || 0) * 22;
-    let dogduplo = (parseInt(document.getElementById("inputcqd").value) || 0) * 26;
-    let xsalada = (parseInt(document.getElementById("inputxs").value) || 0) * 29;
-    let refrip = (parseInt(document.getElementById("inputr").value) || 0) * 5;
-    let refri1 = (parseInt(document.getElementById("inputr1").value) || 0) * 8;
+
+    let v1 = document.getElementById("inputcqb").value;
+    let v2 = document.getElementById("inputcqd").value;
+    let v3 = document.getElementById("inputxs").value;
+    let v4 = document.getElementById("inputr").value;
+    let v5 = document.getElementById("inputr1").value;
+
+    if (isNaN(v1) || isNaN(v2) || isNaN(v3) || isNaN(v4) || isNaN(v5)) {
+        alert("Digite apenas números!");
+        return;
+    }
+
+    let dogbasico = (parseFloat(v1) || 0) * 22;
+    let dogduplo = (parseFloat(v2) || 0) * 26;
+    let xsalada = (parseFloat(v3) || 0) * 29;
+    let refrip = (parseFloat(v4) || 0) * 5;
+    let refri1 = (parseFloat(v5) || 0) * 8;
 
     let resp = dogbasico + dogduplo + xsalada + refrip + refri1;
-    if (resp<0){
-        resp = 0 
-    }
 
     document.getElementById("total").innerHTML = "R$ " + resp.toFixed(2);
 }
