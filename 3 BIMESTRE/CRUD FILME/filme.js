84cb0f47e8fe41712b39e91c6d5aa37ae07a2186
Larasalonskii <laraszanoni14@gmail.com>
2026-08-11 11:44:01 -0300

    add cruds

---
+++ 3 BIMESTRE/CRUD FILME/filme.js
@@ -0,0 +1,12 @@
+class Filme { 
+constructor (id,nome,diretor,dataLancamento,duracao, posicaoNaLista) {
+this.id = id;
+this.nome = nome;
+this.diretor = diretor;
+this.dataLancamento = dataLancamento;
+this.duracao = duracao;
+
+
+this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+}
+}
